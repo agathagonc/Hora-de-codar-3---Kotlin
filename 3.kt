bea50a5d82752158ba76446d2c6746e91e7775cd
Agatha Gonçalves <agathagoncalves002@gmail.com>
2025-03-17 19:21:48 -0300

    Add files via upload

---
+++ 3.kt
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    var n1: Int
+    n1 = 10
+
+    while(n1 > 0 ){
+        println(n1)
+        n1--
+
+
+    }
+}

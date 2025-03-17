09909ed796ea8a970aa20b5ee5b4b8d288111499
Agatha Gonçalves <agathagoncalves002@gmail.com>
2025-03-17 19:15:39 -0300

    Add files via upload

---
+++ 2.kt
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    var n1: Int
+
+    n1 = 30
+
+    while (n1 > -1) {
+        println("Falta $n1 para explodir!!")
+        n1 = n1 - 1
+
+    print("EXPLOSÃO")
+}
+}

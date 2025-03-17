da9ca8bd68b55cb3d30a5aaa7c2f58df87caa5e4
Agatha Gonçalves <agathagoncalves002@gmail.com>
2025-03-17 19:42:47 -0300

    Add files via upload

---
+++ 9.kt
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    // Escreva um algoritmo para imprimir os 10 primeiros números inteiros maiores que 100.
+    var contador: Int
+    contador = 0
+    var numero: Int
+    numero = 101
+
+    while (contador < 10)
+    {
+        println(numero)
+        numero++
+        contador++
+    }
+
+}
